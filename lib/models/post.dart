338dcff562222c10818070897b015457adf3a273
vlad_popa <vlad.gabriel.popa98@gmail.com>
2020-04-25 14:53:07 +0300

    Home wrapper

---
+++ lib/models/post.dart
@@ -0,0 +1,16 @@
+class Post {
+  String pid;
+  String title;
+  String description;
+  String uid;
+  String imageName;
+
+  Post(pid, title, description, uid, imageName) {
+    this.pid = pid;
+    this.title = title;
+    this.description = description;
+    this.uid = uid;
+    this.imageName = imageName;
+
+  }
+}
